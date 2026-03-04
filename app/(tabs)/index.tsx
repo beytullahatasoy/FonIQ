b5d93967db3736ce339e812edc745844e63074e9
Beytullah Atasoy <beytullahatasoy55@gmail.com>
2026-03-04 18:12:24 +0300

    fonIQ

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function KesfetScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Keşfet</Text>
+    </View>
+  );
+}
