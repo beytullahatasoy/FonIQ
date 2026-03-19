86290ec7d8f099fed8a69c526723b4560c027300
Beytullah Atasoy <beytullahatasoy55@gmail.com>
2026-03-19 15:30:49 +0300

    layout update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,36 @@
 import { Tabs } from "expo-router";
+import { FontAwesome6 } from "@expo/vector-icons";
 
 export default function Layout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Keşfet" }} />
-      <Tabs.Screen name="compare" options={{ title: "Karşılaştır" }} />
-      <Tabs.Screen name="ai" options={{ title: "AI" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Keşfet",
+          tabBarIcon: () => (
+            <FontAwesome6 name="searchengin" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="compare"
+        options={{
+          title: "Karşılaştır",
+          tabBarIcon: () => (
+            <FontAwesome6 name="code-compare" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="ai"
+        options={{
+          title: "AI",
+          tabBarIcon: () => (
+            <FontAwesome6 name="message" size={24} color="black" />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
